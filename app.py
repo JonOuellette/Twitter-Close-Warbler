0030df7c3ffb963948863672a7b305b4f684b093
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-09-29 18:38:51 -0700

    build: update the cdn links for bootstrap, fontawesome and popper to current versions. feat: changed thumbs up icon for likes to hearts. Set route to link messages and users for likes.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, flash, redirect, session, g
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
@@ -221,6 +221,28 @@ def show_likes(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('users/likes.html', user=user, likes=user.likes)
 
+@app.route('/messages/<int:message_id>/like', methods=['POST'])
+def add_like(message_id):
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    liked_message= Message.query.get_or_404(message_id)
+    if liked_message.user_id == g.user.id:
+        return abort(403)
+    
+    user_likes = g.user.likes
+
+    if liked_message in user_likes:
+        g.user.likes = [like for like in user_likes if like != liked_message]
+    
+    else:
+        g.user.likes.append(liked_message)
+
+    db.session.commit()
+
+    return redirect("/")
 
 @app.route('/users/profile', methods=['GET', 'POST'])
 def edit_profile():
